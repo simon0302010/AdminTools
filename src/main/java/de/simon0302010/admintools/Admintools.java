3757002e17bdbaa942447b7f42a0b01bde7e2c62
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2025-10-06 17:14:57 +0200

    Basic Mod with /locateplayer command.

---
+++ src/main/java/de/simon0302010/admintools/Admintools.java
@@ -0,0 +1,20 @@
+package de.simon0302010.admintools;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.server.command.CommandManager;
+
+public class Admintools implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            dispatcher.register(CommandManager.literal("locateplayer")
+                    .requires(source -> source.hasPermissionLevel(1))
+                    .then(CommandManager.argument("player", EntityArgumentType.player())
+                            .executes(PlayerLocator::getLocation)
+                    )
+            );
+        });
+    }
+}
---
+++ src/main/java/de/simon0302010/admintools/PlayerLocator.java
@@ -0,0 +1,27 @@
+package de.simon0302010.admintools;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.text.Text;
+
+public class PlayerLocator
+{
+    static int getLocation(CommandContext<ServerCommandSource> context) {
+        try {
+            ServerPlayerEntity player = EntityArgumentType.getPlayer(context, "player");
+            String playerWorld = player.getWorld().getRegistryKey().getValue().toString().replaceFirst("^minecraft:", "").replaceFirst("^the_", "");
+            context.getSource().sendFeedback(
+                    () -> Text.literal(
+                            "%s is at X=%.2f, Y=%.2f, Z=%.2f in %s".formatted(player.getName().getString(), player.getX(), player.getY(), player.getZ(), playerWorld.substring(0,1).toUpperCase() + playerWorld.substring(1).toLowerCase())
+                    ),
+                    false
+            );
+        } catch (CommandSyntaxException e) {
+            context.getSource().sendError(Text.literal("Player not found."));
+        }
+        return 1;
+    }
+}
